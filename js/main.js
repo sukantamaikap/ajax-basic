57d72760abfd4bbabbde9e21a6bd52cf63e58141
sukantamaikap <sukanta.maikap@gmail.com>
2020-04-01 14:47:57 +1100

    working page update based on click

--- js/main.js
+++ js/main.js
@@ -1,10 +1,28 @@
+var pageCount = 1;
+var container = document.getElementById('animal-info');
+
 var btn = document.getElementById('btn');
 btn.addEventListener('click', function() {
     var request = new XMLHttpRequest(), data;
-    request.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.json');
+    request.open('GET', 'https://learnwebcode.github.io/json-example/animals-' + pageCount + '.json');
     request.onload = function() {
         data = JSON.parse(request.responseText);
-        console.log(data[0]);
+        renderHTML(data);
     };
 request.send();
+pageCount++;
+
+if(pageCount > 3) {
+    btn.classList.add("hide-me");
+}
 });
+
+
+function renderHTML(data) {
+    // add html to empty div element
+    var htmlString = '', i = 0;
+    for (; i < data.length; i++) {
+        htmlString += '<p>' + data[i].name + ' is a ' + data[i].species + '</p>';
+    }
+     container.insertAdjacentHTML('beforeend', htmlString);
+}
