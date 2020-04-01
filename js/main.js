e596c0659398109e2ba861b8dea7bf077980026b
sukantamaikap <sukanta.maikap@gmail.com>
2020-04-01 13:49:57 +1100

    adding event listener for the fetch button

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
-var request = new XMLHttpRequest(), data;
-request.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.json');
-request.onload = function() {
-    data = request.responseText;
-    console.log(data);
-};
-
+var btn = document.getElementById('btn');
+btn.addEventListener('click', function() {
+    var request = new XMLHttpRequest(), data;
+    request.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.json');
+    request.onload = function() {
+        data = JSON.parse(request.responseText);
+        console.log(data[0]);
+    };
 request.send();
+});
